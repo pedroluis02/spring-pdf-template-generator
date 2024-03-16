6b458966aafee261341668153e61519812075415
Pedro Luis <pedroluisnr22@gmail.com>
2024-03-16 10:19:14 -0500

    build: :package: add zxing library
    Zxing library v3.5.3 for qr code generation
    f

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,49 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.3"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "com.github.pedroluis02.springbootsamples"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	val itextPdfVersion = "5.0.3"
-	val xhtmlRendererVersion = "9.5.2"
+    val itextPdfVersion = "5.0.3"
+    val xhtmlRendererVersion = "9.5.2"
+    val zxingVersion = "3.5.3"
 
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-	implementation("com.itextpdf:html2pdf:$itextPdfVersion")
-	implementation("org.xhtmlrenderer:flying-saucer-pdf:$xhtmlRendererVersion")
+    implementation("com.itextpdf:html2pdf:$itextPdfVersion")
+    implementation("org.xhtmlrenderer:flying-saucer-pdf:$xhtmlRendererVersion")
+    
+    implementation("com.google.zxing:javase:$zxingVersion")
 
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "21"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "21"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
