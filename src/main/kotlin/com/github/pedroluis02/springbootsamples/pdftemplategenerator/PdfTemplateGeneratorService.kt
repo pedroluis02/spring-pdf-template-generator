03b7bd153a768e5bcdb5e2a2066c8036a388aefa
Pedro Luis <pedroluisnr22@gmail.com>
2024-03-15 20:54:10 -0500

    feat :sparkles: generate pdf with flying template

---
+++ src/main/kotlin/com/github/pedroluis02/springbootsamples/pdftemplategenerator/PdfTemplateGeneratorService.kt
@@ -0,0 +1,46 @@
+package com.github.pedroluis02.springbootsamples.pdftemplategenerator
+
+
+import org.springframework.stereotype.Service
+import org.thymeleaf.TemplateEngine
+import org.thymeleaf.context.Context
+import org.xhtmlrenderer.pdf.ITextRenderer
+import java.io.ByteArrayOutputStream
+import java.io.FileOutputStream
+import java.io.OutputStream
+
+@Service
+class PdfTemplateGeneratorService(private val templateEngine: TemplateEngine) {
+
+    fun generateFile(template: String, data: Map<String, Any>, output: String) {
+        val outputStream = FileOutputStream(output)
+        generate(template, data, outputStream)
+    }
+
+    fun generateStream(template: String, data: Map<String, Any>): ByteArrayOutputStream {
+        val outputStream = ByteArrayOutputStream()
+        generate(template, data, outputStream)
+        return outputStream
+    }
+
+    private fun generate(template: String, data: Map<String, Any>, outputStream: OutputStream) {
+        val context = Context()
+        context.setVariables(data)
+
+        val processedTemplate = templateEngine.process(template, context)
+        return convert(processedTemplate, outputStream)
+    }
+
+    private fun convert(processedTemplate: String, outputStream: OutputStream) {
+        try {
+            val renderer = ITextRenderer().apply {
+                setDocumentFromString(processedTemplate)
+                layout()
+            }
+
+            renderer.createPDF(outputStream, true)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+}
